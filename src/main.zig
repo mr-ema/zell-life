16931284ac343339bbec0c56ab6b9823d562ebd6
mr-ema <ema.a.v.c2017@gmail.com>
2023-06-25 13:08:07 -0400

    feat: allow zoom and move in the world

--- src/main.zig
+++ src/main.zig
@@ -2,10 +2,10 @@ const raylib = @import("raylib");
 const std = @import("std");
 const rand = std.rand;
 
-const SCREEN_WIDTH = 500;
-const SCREEN_HEIGHT = 500;
+const SCREEN_WIDTH = 800;
+const SCREEN_HEIGHT = 600;
 const CELL_SIZE = 4;
-const TARGET_FPS = 60;
+const TARGET_FPS = 20;
 
 const CellState = enum { Dead, Alive };
 const Vector2 = struct { x: usize, y: usize };
@@ -91,22 +91,50 @@ pub fn main() void {
 
     defer raylib.CloseWindow();
 
+    var camera: raylib.Camera2D = undefined;
+    camera.zoom = 1.0;
+
     var grid = Grid(SCREEN_HEIGHT / (CELL_SIZE / 2), SCREEN_WIDTH / (CELL_SIZE / 2)).init();
 
     while (!raylib.WindowShouldClose()) {
+        // Translate based on mouse right click
+        if (raylib.IsMouseButtonDown(.MOUSE_BUTTON_RIGHT)) {
+            var delta = raylib.GetMouseDelta();
+            delta = raylib.Vector2Scale(delta, -1.0 / camera.zoom);
+
+            camera.target = raylib.Vector2Add(camera.target, delta);
+        }
+
+        // Zoom based on mouse wheel
+        var wheel: f32 = raylib.GetMouseWheelMove();
+        if (wheel != 0) {
+            var mouseWorldPos = raylib.GetScreenToWorld2D(raylib.GetMousePosition(), camera);
+
+            camera.offset = raylib.GetMousePosition();
+
+            camera.target = mouseWorldPos;
+
+            const zoomIncrement: f32 = 0.125;
+            camera.zoom += (wheel * zoomIncrement);
+            if (camera.zoom < zoomIncrement) {
+                camera.zoom = zoomIncrement;
+            }
+        }
+
         raylib.BeginDrawing();
         defer raylib.EndDrawing();
 
         raylib.ClearBackground(raylib.RAYWHITE);
-        raylib.DrawFPS(10, 10);
 
+        raylib.BeginMode2D(camera);
         for (grid.grid, 0..) |row, y| {
             for (row, 0..) |state, x| {
                 if (state == CellState.Alive) {
-                    raylib.DrawRectangle(@intCast(i32, x * CELL_SIZE / 2), @intCast(i32, y * CELL_SIZE / 2), CELL_SIZE, CELL_SIZE, raylib.BLACK);
+                    raylib.DrawRectangle(@intCast(i32, x * CELL_SIZE), @intCast(i32, y * CELL_SIZE), CELL_SIZE, CELL_SIZE, raylib.BLACK);
                 }
             }
         }
+        raylib.EndMode2D();
 
         grid.update();
     }
